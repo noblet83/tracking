64b104b2a1de4fdac02bb0dc1743a8adcbde4a06
noblet83 <thomas.m.noble@gmail.com>
2014-11-27 06:19:24 -0500

    first commit

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "tracking");
+define("DB_PASS", "meatball");
+define("DB_NAME", "tracking");
+
+define("SITE_TITLE", "Tracking DB");
+
+//Paths
+define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/tracking/');
